529e7d040995a45017849a22fc014d88e6b0b645
Aleksander Chumachenko <ledich19@gmail.com>
2023-12-01 22:32:25 +0200

    Delete src/Components directory

--- src/Components/index.ts
+++
@@ -1,8 +0,0 @@
-export { Error } from './Error/Error';
-// export {ProductList } from './Products/ProductList/ProductList';
-export { Header } from './Header/Header';
-export { UsersList } from './Users/UsersList';
-export { Loader } from './Loader';
-export { OrdersList } from './Orders/OrdersList';
-export { CategoriesList } from './Categories/CategoriesList';
-export { CategoryForm } from './Categories/CategoryForm';
