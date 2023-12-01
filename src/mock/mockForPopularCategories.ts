645c6e6e8faf0fe66227a2c04112208d922ab7fb
Aleksander Chumachenko <ledich19@gmail.com>
2023-12-01 22:18:55 +0200

    Add files via upload

---
+++ src/mock/mockForPopularCategories.ts
@@ -0,0 +1,48 @@
+import {Category} from "@/component/PopularCategories/PopularCategories";
+
+const mockCategories:Category[] = [
+    {
+        id: 1,
+        label: "Люкс",
+        icon: "luxury",
+        link: 'categoryLink'
+    },
+    {
+        id: 2,
+        label: "Бренди",
+        icon: "brands",
+        link: 'categoryLink'
+    },
+    {
+        id: 3,
+        label: "Парфумерія",
+        icon: "perfume",
+        link: 'categoryLink'
+    },
+    {
+        id: 4,
+        label: "Макіяж",
+        icon: "makeup",
+        link: 'categoryLink'
+    },
+    {
+        id: 5,
+        label: "Волосся",
+        icon: "hair",
+        link: 'categoryLink'
+    },
+    {
+        id: 6,
+        label: "Догляд",
+        icon: "body",
+        link: 'categoryLink'
+    },
+    {
+        id: 7,
+        label: "Дитячі товари",
+        icon: "kids",
+        link: 'categoryLink'
+    }
+];
+
+export default mockCategories;
